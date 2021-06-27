9c54405c951c8f56d09af758387754a9365feec3
slbb <7777777yzy@gmail.com>
2021-06-28 00:33:13 +0800

    change workflow

--- compile.sh
+++
@@ -1,8 +0,0 @@
-#!/c/msys64/mingw64
-# Compile hex
-qmk json2c mykeymap.json
-mkdir ./qmk_firmware/keyboards/wheatfield/split75/bx
-mv ./keymap.c ./qmk_firmware/keyboards/wheatfield/split75/bx/keymap.c
-mv ./rules.mk ./qmk_firmware/keyboards/wheatfield/split75/bx/rules.mk
-qmk compile -kb wheatfield/split75 -km bx
-ls
